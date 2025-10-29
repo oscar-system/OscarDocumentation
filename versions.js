eee82a2d46fd099bda33373cfaac678964a7a800
Max Horn <max@quendi.de>
2025-10-29 16:09:03 +0000

    build based on 1b6221f

--- versions.js
+++ versions.js
@@ -1,5 +1,14 @@
 var DOC_VERSIONS = [
   "stable",
+  "v1.5",
+  "v1.4",
+  "v1.3",
+  "v1.2",
+  "v1.1",
+  "v1.0",
+  "v0.14",
+  "v0.13",
+  "v0.12",
   "v0.11",
   "v0.10",
   "v0.9",
@@ -12,5 +21,5 @@ var DOC_VERSIONS = [
   "v0.2",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v0.11.2";
+var DOCUMENTER_NEWEST = "v1.5.0";
 var DOCUMENTER_STABLE = "stable";
